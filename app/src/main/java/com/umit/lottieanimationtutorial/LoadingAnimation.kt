217af933ab8e2c65508c3e6c934f8d4e07d1521c
Antalyaspor <kaya_umit_07@msn.com>
2023-05-02 00:18:54 +0300

    first commit

---
+++ app/src/main/java/com/umit/lottieanimationtutorial/LoadingAnimation.kt
@@ -0,0 +1,39 @@
+package com.umit.lottieanimationtutorial
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun LoadingAnimation() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.progress))
+    var isPlaying by remember{ mutableStateOf(true) }
+    val progress by animateLottieCompositionAsState(composition, isPlaying = isPlaying,)
+    LaunchedEffect(key1 = progress){
+        if (progress==0f){isPlaying=true}
+        if (progress==1f){isPlaying=false}
+    }
+    Column(modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        isPlaying=true
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier.size(100.dp))
+    }
+}
---
+++ app/src/main/java/com/umit/lottieanimationtutorial/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.umit.lottieanimationtutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.umit.lottieanimationtutorial.ui.theme.LottieAnimationTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LottieAnimationTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoadingAnimation()
+                }
+            }
+        }
+    }
+}
+
+
